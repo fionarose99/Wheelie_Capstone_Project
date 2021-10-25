a534b66cffbc94ac6aac805a3454ad24d2bf14b0
Fiona Rose <fiona.r.murphey@gmail.com>
2021-10-25 19:08:55 -0400

    Camera Follow Wheelie Model

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-
+    public Camera maincam;
     public float speed = 1;
-    private Rigidbody2D rb;
+
+    private Rigidbody2D WheelieRB;
+    // Addresses rb attached to Wheelie Prefab
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        WheelieRB = GetComponent<Rigidbody2D>();
+        maincam = Camera.main;
     }
 
-    // Update is called once per frame
+    // FixedUpdate called first -- Once per frame
     void FixedUpdate()
     {
         float move = Input.GetAxis("Horizontal") * speed;
+        // Horizontal Axis contains input info: left (a/arrow), right (d/arrow)
 
         //transform.Translate((move,0,0))
 
-        rb.AddForce(new Vector2(move, 0f));
+        WheelieRB.AddForce(new Vector2(move, 0f));
         Debug.Log(move);
+    }
+
+    // Update called second -- once per frame
+    private void Update()
+    {
+        maincam.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        // Default transform.position regards whatever object it's attached to -- the GameObject's transform (here, the Wheelie prefab)
     }
 }
