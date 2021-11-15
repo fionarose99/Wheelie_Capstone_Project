50bd2cd0fcf8074b60d1119dbc8dd3eedafc9fdb
ns0906a <ns0906a@student.american.edu>
2021-11-15 06:17:44 -0500

    Added Basic Momentum Controls
    Seperated the Wheel from the Body, make the wheel controllable. The player can now flip, move and brake.

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -4,29 +4,76 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    public Camera maincam;
-    public float speed = 1;
+    Camera maincam;
 
+    // Physics Variables
+    public float speed = 1;
+    public float maxForce = 10000;
+    public WheelJoint2D Wheel;
+    private JointMotor2D motor;
     private Rigidbody2D WheelieRB;
-    // Addresses rb attached to Wheelie Prefab
+    bool motorOn;
+    float move;
+
+    // Animation Variables
+    private Animator ani;
+    bool isLeft = true;
+
+    // Horizontal Axis contains input info: left (a/arrow), right (d/arrow)
 
     // Start is called before the first frame update
     void Start()
     {
-        WheelieRB = GetComponent<Rigidbody2D>();
-        maincam = Camera.main;
+        ani = GetComponent<Animator>();             // Addresses Animator attached to Wheelie Prefab
+        WheelieRB = GetComponent<Rigidbody2D>();    // Addresses Rigibody attached to Wheelie Prefab
+        maincam = Camera.main;                      // Scene Main Camera
+        motor.maxMotorTorque = maxForce;            // Set's max force the player can add to the Wheelchair
     }
 
     // FixedUpdate called first -- Once per frame
     void FixedUpdate()
     {
-        float move = Input.GetAxis("Horizontal") * speed;
-        // Horizontal Axis contains input info: left (a/arrow), right (d/arrow)
+        float dir = Input.GetAxis("Horizontal");    // Get Horizonal directional input (A or D/Left or Right)
+        if (dir != 0)                               // What happens when the player is moving
+        {
+            move += dir * speed * Time.deltaTime;   // Add force to movement
+            motorOn = true;                         // Turn on Wheel Motor
+            if ((dir > move && dir > 0) || (dir < move && dir < 0))     // Braking Code
+            {
+                ani.SetBool("Braking", true); // If the player is adding force to the opposite direction they are facing, play Braking animation.
+            }
+            else
+            {
+                ani.SetBool("Braking", false); // Otherwise don't.
+            }
+        }
+        else                                         // When the player stops pressing inputs, turn off motor and decrease motor speed. This makes the wheel go slack and allows for rolling down hills.
+        {
+            motorOn = false;
+            move *= 0.9f;
 
-        //transform.Translate((move,0,0))
+            // This is a very simple momentum. It does not track the momentum of the wheel so whenever the player starts moving again, they may accidentally trip.
+        }
 
-        WheelieRB.AddForce(new Vector2(move, 0f));
-        Debug.Log(move);
+        if (isLeft && move > 0 && dir > 0)              // Flip code. If the player switches which way they are moving, they spin.
+        {
+            isLeft = false;
+            ani.SetTrigger("Flip");
+        } else if (!isLeft && move < 0 && dir < 0)
+        {
+            isLeft = true;
+            ani.SetTrigger("Flip");
+        }
+
+        // Adds the motor information (it's speed and whether or not it is on).
+
+        motor.motorSpeed = move;
+        Wheel.motor = motor;
+        Wheel.useMotor = motorOn;
+        ani.SetBool("Moving", motorOn);
+        
+        // Debug for testing
+        Debug.Log(dir + "," + move);
     }
 
     // Update called second -- once per frame
