a8ffe333de9c9e075c5a8b1e51aef398fc1de3b5
ns0906a <ns0906a@student.american.edu>
2021-11-15 16:28:22 -0500

    Added More Physics Abilities and Experimented with Tileset
    The Tileset is actually somewhat limiting. We should see if there are other ways to build the level and add bumps and stuff.

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -8,12 +8,17 @@ public class MovementController : MonoBehaviour
 
     // Physics Variables
     public float speed = 1;
+    public float leanSpeed = 1;
     public float maxForce = 10000;
     public WheelJoint2D Wheel;
-    private JointMotor2D motor;
-    private Rigidbody2D WheelieRB;
-    bool motorOn;
+    private JointMotor2D Wheelmotor;
+    bool WheelmotorOn;
+    public HingeJoint2D joint;
+    private JointMotor2D JointMot;
+    bool JointMotorOn;
     float move;
+    float lean;
+    float direction = 1;
 
     // Animation Variables
     private Animator ani;
@@ -25,19 +30,31 @@ public class MovementController : MonoBehaviour
     void Start()
     {
         ani = GetComponent<Animator>();             // Addresses Animator attached to Wheelie Prefab
-        WheelieRB = GetComponent<Rigidbody2D>();    // Addresses Rigibody attached to Wheelie Prefab
         maincam = Camera.main;                      // Scene Main Camera
-        motor.maxMotorTorque = maxForce;            // Set's max force the player can add to the Wheelchair
+        Wheelmotor.maxMotorTorque = maxForce;       // Set's max force the player can add to the Wheelchair
+        JointMot.maxMotorTorque = maxForce;       // Set's max force the player can add to the Wheelchair
     }
 
     // FixedUpdate called first -- Once per frame
     void FixedUpdate()
     {
+        float jump = Input.GetAxis("Jump");
+        if (jump != 0)
+        {
+            lean += jump * leanSpeed * Time.deltaTime * direction;
+            move *= 1.05f;
+            JointMotorOn = true;
+        } else
+        {
+            JointMotorOn = false;
+            lean *= 0.90f;
+        }
+
         float dir = Input.GetAxis("Horizontal");    // Get Horizonal directional input (A or D/Left or Right)
         if (dir != 0)                               // What happens when the player is moving
         {
             move += dir * speed * Time.deltaTime;   // Add force to movement
-            motorOn = true;                         // Turn on Wheel Motor
+            WheelmotorOn = true;                         // Turn on Wheel Motor
             if ((dir > move && dir > 0) || (dir < move && dir < 0))     // Braking Code
             {
                 ani.SetBool("Braking", true); // If the player is adding force to the opposite direction they are facing, play Braking animation.
@@ -49,7 +66,7 @@ public class MovementController : MonoBehaviour
         }
         else                                         // When the player stops pressing inputs, turn off motor and decrease motor speed. This makes the wheel go slack and allows for rolling down hills.
         {
-            motorOn = false;
+            WheelmotorOn = false;
             move *= 0.9f;
 
             // This is a very simple momentum. It does not track the momentum of the wheel so whenever the player starts moving again, they may accidentally trip.
@@ -59,18 +76,23 @@ public class MovementController : MonoBehaviour
         {
             isLeft = false;
             ani.SetTrigger("Flip");
+            direction = -1;
         } else if (!isLeft && move < 0 && dir < 0)
         {
             isLeft = true;
             ani.SetTrigger("Flip");
+            direction = 1;
         }
 
         // Adds the motor information (it's speed and whether or not it is on).
 
-        motor.motorSpeed = move;
-        Wheel.motor = motor;
-        Wheel.useMotor = motorOn;
-        ani.SetBool("Moving", motorOn);
+        Wheelmotor.motorSpeed = move;
+        JointMot.motorSpeed = lean;
+        Wheel.motor = Wheelmotor;
+        joint.motor = JointMot;
+        Wheel.useMotor = WheelmotorOn;
+        joint.useMotor = JointMotorOn;
+        ani.SetBool("Moving", WheelmotorOn);
         
         // Debug for testing
         Debug.Log(dir + "," + move);
