8338be0f4ba8796cce755e736924daf8a9c7025d
Fiona Rose <fiona.r.murphey@gmail.com>
2022-05-01 20:52:57 -0400

    Inventory Script created, attached to Player

---
+++ Assets/InventoryMan.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMan : MonoBehaviour
+{
+    public int sigCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // OnTriggerEnter -- tag = "Signature"
+    }
+}
